2031fefdbd799b33d5197eba2b96f724dd639f9f
chaosflaws <0175958+chaosflaws@users.noreply.github.com>
2024-03-09 20:10:14 +0100

    progressive enhance with js async form submit

--- src/drink_selector/drink_selector.mjs
+++ src/drink_selector/drink_selector.mjs
@@ -1,1 +1,14 @@
-console.log('hello');
+const drink_grid = document.getElementById('drink-grid');
+const buttons = drink_grid.getElementsByTagName('button');
+for (const button of buttons) {
+    button.onclick = function (e) {
+        e.preventDefault();
+        fetch('add_order', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: button.name + '=' + button.value
+        });
+    }
+}
