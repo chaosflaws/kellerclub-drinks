5b6d1f2838d767eaadbfd1f886e3970454bbf4ff
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-03-17 11:58:42 +0100

    add ajax response creator

--- src/kellerclub_drinks/response_creators.py
+++ src/kellerclub_drinks/response_creators.py
@@ -1,9 +1,22 @@
 """
 Interface and implementations of response creators.
 """
-
+import json
 from abc import ABC, abstractmethod
+from typing import Any
 from wsgiref.types import StartResponse
+
+
+_STATUS_MESSAGES = {
+    200: 'OK',
+    303: 'See Other',
+    400: 'Bad Request',
+    404: 'Not Found'
+}
+
+
+def _get_status_string(status_code: int):
+    return f'{status_code} {_STATUS_MESSAGES[status_code]}'
 
 
 class ResponseCreator(ABC):
@@ -23,11 +36,6 @@ class ErrorCreator(ResponseCreator):
     registered. Otherwise, sends a generic code 400 error message.
     """
 
-    STATUS_CODES = {
-        400: 'Bad Request',
-        404: 'Not Found'
-    }
-
     def __init__(self, status_code: int):
         self.status_code = status_code if 400 <= status_code < 500 else 400
 
@@ -35,7 +43,7 @@ class ErrorCreator(ResponseCreator):
         with open(f'./kellerclub_drinks/{self.status_code}.html', 'rb') as error_file:
             content = error_file.read()
 
-        status = f'{self.status_code} {self.STATUS_CODES[self.status_code]}'
+        status = _get_status_string(self.status_code)
         response_headers = [('Content-type', 'text/html; charset=utf-8'),
                             ('Content-Length', str(len(content)))]
         start_response(status, response_headers)
@@ -50,7 +58,7 @@ class SuccessCreator(ResponseCreator):
         self.content = content
 
     def serve(self, start_response: StartResponse) -> list[bytes]:
-        status = '200 OK'
+        status = _get_status_string(200)
         response_headers = [('Content-type', self.content_type),
                             ('Content-Length', str(len(self.content)))]
         start_response(status, response_headers)
@@ -63,6 +71,21 @@ class HtmlCreator(SuccessCreator):
         super().__init__('text/html; charset=utf-8', content)
 
 
+class AjaxCreator(ResponseCreator):
+    """Serves an ajax response."""
+
+    def __init__(self, status_code: int, response: Any):
+        self.status_code = status_code
+        self.response = json.dumps(response)
+
+    def serve(self, start_response: StartResponse) -> list[bytes]:
+        status = _get_status_string(self.status_code)
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(self.response)))]
+        start_response(status, response_headers)
+        return [self.response.encode()]
+
+
 class RedirectCreator(ResponseCreator):
     """Serves an HTTP response containing a generic redirect."""
 
@@ -70,7 +93,7 @@ class RedirectCreator(ResponseCreator):
         self.new_path = new_path
 
     def serve(self, start_response: StartResponse) -> list[bytes]:
-        status = '303 See Other'
+        status = _get_status_string(303)
         response_headers = [('Location', self.new_path)]
         start_response(status, response_headers)
         return []
