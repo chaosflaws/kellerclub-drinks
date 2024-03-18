be29ddfc1962b2463459485811b404d376385a75
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-03-18 16:22:29 +0100

    make jinja2 parsing more strict

--- src/kellerclub_drinks/resources.py
+++ src/kellerclub_drinks/resources.py
@@ -1,6 +1,6 @@
 """Provides global resources to the application."""
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 from .datastores import datastore_factory
 from .datastores.datastore import DataStore
@@ -10,4 +10,6 @@ from .settings import Settings
 class Resources:
     def __init__(self, settings: Settings):
         self.datastore: DataStore = datastore_factory.from_settings(settings.data_store_settings)
-        self.jinjaenv = Environment(loader=FileSystemLoader("kellerclub_drinks/handlers"), autoescape=True)
+        self.jinjaenv = Environment(loader=FileSystemLoader("kellerclub_drinks/handlers"),
+                                    autoescape=True,
+                                    undefined=StrictUndefined)
