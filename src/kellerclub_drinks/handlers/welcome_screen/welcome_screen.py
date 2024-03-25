24b769b1fbd5d5881920203f84ce3bf4a335a2d4
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-03-26 00:22:55 +0100

    add different welcome screen for while an event is running

--- src/kellerclub_drinks/handlers/welcome_screen/welcome_screen.py
+++ src/kellerclub_drinks/handlers/welcome_screen/welcome_screen.py
@@ -6,6 +6,12 @@ from ...templates import render_template
 
 class WelcomeScreen(ResistantHandler):
     def _handle(self, res: Resources) -> ResponseCreator:
-        content = render_template(res.jinjaenv,
-                                  'welcome_screen/welcome_screen.jinja2')
+        if event := res.datastore.current_event():
+            content = render_template(res.jinjaenv,
+                                      'welcome_screen/welcome_screen_running_event.jinja2',
+                                      event=event)
+        else:
+            content = render_template(res.jinjaenv,
+                                      'welcome_screen/welcome_screen_no_event.jinja2')
+
         return HtmlCreator().with_content(content.encode())
