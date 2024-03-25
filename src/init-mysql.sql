baedf3f438e01011f1280305a18e92efac44d6fd
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-03-26 00:20:39 +0100

    fix MySQL foreign key syntax

--- src/init-mysql.sql
+++ src/init-mysql.sql
@@ -11,8 +11,11 @@ CREATE TABLE Event (
 
 CREATE TABLE PurchaseOrder (
     time TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP PRIMARY KEY,
-    drink_name VARCHAR(100) NOT NULL
-        REFERENCES Drink(name)
+    drink_name VARCHAR(100) NOT NULL,
+    /*event TIMESTAMP NOT NULL,*/
+
+    FOREIGN KEY (drink_name) REFERENCES Drink(name)
+    /*FOREIGN KEY (event) REFERENCES Event(start_time)*/
 );
 
 CREATE TABLE SelectorLayout (
@@ -21,23 +24,26 @@ CREATE TABLE SelectorLayout (
 
 CREATE TABLE SelectorButton (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    layout_name VARCHAR(100) NOT NULL
-        REFERENCES SelectorLayout(name),
+    layout_name VARCHAR(100) NOT NULL,
     xpos INTEGER NOT NULL,
     ypos INTEGER NOT NULL,
-    display_name TEXT
+    display_name TEXT,
+
+    FOREIGN KEY (layout_name) REFERENCES SelectorLayout(name)
 );
 
 CREATE TABLE OrderButton (
-    button_id INTEGER NOT NULL
-        REFERENCES SelectorButton(id),
-    drink_name VARCHAR(100) NOT NULL
-        REFERENCES Drink(name)
+    button_id INTEGER NOT NULL,
+    drink_name VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (button_id) REFERENCES SelectorButton(id),
+    FOREIGN KEY (drink_name) REFERENCES Drink(name)
 );
 
 CREATE TABLE LinkButton (
-    button_id INTEGER NOT NULL
-        REFERENCES SelectorButton(id),
-    linked_layout VARCHAR(100) NOT NULL
-        REFERENCES SelectorLayout(name)
+    button_id INTEGER NOT NULL,
+    linked_layout VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (button_id) REFERENCES SelectorButton(id),
+    FOREIGN KEY (linked_layout) REFERENCES SelectorLayout(name)
 );
