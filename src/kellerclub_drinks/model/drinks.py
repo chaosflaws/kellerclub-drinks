8bd321f406c90a8c5384086c1afc8b6c86d11005
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-04-14 21:56:53 +0200

    restrict internal drink names to non-empty ascii letters, digits and underscores

--- src/kellerclub_drinks/model/drinks.py
+++ src/kellerclub_drinks/model/drinks.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 
 
@@ -7,3 +8,13 @@ class Drink:
 
     name: str
     display_name: str
+
+    def __post_init__(self) -> None:
+        if not Drink.valid_name(self.name):
+            raise ValueError('Invalid drink name!')
+
+    @staticmethod
+    def valid_name(name: str) -> bool:
+        """True if name is a valid internal name, false otherwise."""
+
+        return bool(re.match('^[a-zA-Z0-9_]+$', name))
