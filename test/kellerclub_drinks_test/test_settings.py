6d5b22d7b0ef9f25e8154f47ca139e8cab8f3fcf
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-04-20 15:29:20 +0200

    test cache-age setting

--- test/kellerclub_drinks_test/test_settings.py
+++ test/kellerclub_drinks_test/test_settings.py
@@ -1,13 +1,25 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring
 # pylint: disable=missing-function-docstring
 
+import json
 import unittest
 from json import JSONDecodeError
+from typing import Any
 
 from kellerclub_drinks.settings import Settings
 
 
 class TestSettings(unittest.TestCase):
-    def test_parse__invalid_json__raises(self) -> None:
+    def test_parse_settings__invalid_json__raises(self) -> None:
         with self.assertRaises(JSONDecodeError):
             Settings._from_json_string('{')
+
+    def test_parse_settings__cache_age_not_set__default_one_day(self) -> None:
+        settings_param: dict[str, Any] = {'datastore': {}}
+
+        settings = Settings._from_json_string(json.dumps(settings_param))
+
+        self.assertEqual(60 * 60 * 24, settings.cache_age)
+
+    def test_parse__read_from_file__succeeds(self) -> None:
+        Settings._from_file('src/settings.json')
