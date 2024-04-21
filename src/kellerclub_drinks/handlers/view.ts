b6fd55e580230f9aa89de5cdf38878789e505d35
chaosflaws <10175958+chaosflaws@users.noreply.github.com>
2024-04-21 14:09:42 +0200

    allow DocumentFragments in Query

--- src/kellerclub_drinks/handlers/view.ts
+++ src/kellerclub_drinks/handlers/view.ts
@@ -1,28 +1,117 @@
-interface Query<T extends Element | Element[]> {
+interface Query<T extends Element | Element[] | DocumentFragment> {
     path(): string;
-    value(): Element | Element[];
+    value(): Element | Element[] | DocumentFragment;
+
     childWithId(id: string): Query<Element>;
+
     oneName(name: string): Query<T>;
-    withName(name: string): Query<Element>;
     childWithName(name: string): Query<Element>;
+
     oneClass(className: string): Query<T>;
     someClasses(className: string): Query<Element[]>;
-    withClass(className: string): Query<Element>;
+    anyClasses(className: string): Query<Element[]>;
+
     oneTag(tag: keyof HTMLElementTagNameMap): Query<T>;
     someTags(tag: keyof HTMLElementTagNameMap): Query<Element[]>;
     anyTags(tag: keyof HTMLElementTagNameMap): Query<Element[]>;
 }
 
+interface ElementQuery<T extends Element | Element[]> extends Query<T> {
+    withId(id: string): Query<Element>;
+
+    withName(name: string): Query<Element>;
+
+    withClass(className: string): Query<Element>;
+
+    withTag(tag: keyof HTMLElementTagNameMap): Query<Element>;
+}
+
 export function Query(): SingleQuery<HTMLElement>;
-export function Query<T extends Element>(nodes: T): SingleQuery<T>;
+export function Query(node: DocumentFragment): FragmentQuery;
+export function Query<T extends Element>(node: T): SingleQuery<T>;
 export function Query<T extends Element>(nodes: T[]): MultiQuery<T>;
-export function Query(nodes?: Element | Element[]) {
+export function Query(nodes?: Element | Element[] | DocumentFragment) {
     if (!nodes) return new SingleQuery<HTMLElement>('[root]');
+    if (nodes instanceof DocumentFragment) return new FragmentQuery(nodeToStr(nodes), nodes);
     if (nodes instanceof Element) return new SingleQuery(nodeToStr(nodes), nodes);
     else return new MultiQuery(nodes.map(node => nodeToStr(node)).join(','), nodes);
 }
 
-class SingleQuery<T extends Element> implements Query<Element> {
+class FragmentQuery implements Query<DocumentFragment> {
+    readonly #node: DocumentFragment;
+    readonly #path: string;
+
+    constructor(path: string, node: DocumentFragment) {
+        this.#node = node;
+        this.#path = path;
+    }
+
+    path(): string {
+        return this.#path;
+    }
+
+    value(): DocumentFragment {
+        return this.#node;
+    }
+
+    childWithId(id: string) {
+        const result = this.#node.getElementById(id);
+        if (result) return new SingleQuery(`[childWithId=${id}]`, result);
+        else throw err(this, `ID ${id} does not exist or is not a child of ${nodeToStr(this.#node)}!`);
+    }
+
+    oneName(name: string) {
+        return this._oneName(name, `[oneName=${name}]`);
+    }
+
+    private _oneName(name: string, path: string) {
+        const result = this.#node.querySelector(`[name=${name}]`);
+        if (result) return new SingleQuery(path, result);
+        else throw err(this, `${name} does not appear as name attribute of a child of ${nodeToStr(this.#node)}!`);
+    }
+
+    childWithName(name: string) {
+        return this._oneName(name, `[childWithName=${name}]`);
+    }
+
+    oneClass(className: string) {
+        const result = this.#node.querySelector(`.${className}`);
+        if (result) return new SingleQuery(this.#path + `[oneClass=${className}]`, result);
+        else throw err(this, `${className} is not a unique class name below ${nodeToStr(this.#node)}!`);
+    }
+
+    someClasses(className: string) {
+        const result = this.#node.querySelectorAll(`.${className}`);
+        if (result.length)
+            return new MultiQuery(this.#path + `[someClasses=${className}]`, [...result]);
+        else throw err(this, `${className} not present!`);
+    }
+
+    anyClasses(className: string) {
+        const result = this.#node.querySelectorAll(`.${className}`);
+        return new MultiQuery(this.#path + `[anyClasses=${className}]`, [...result]);
+    }
+
+    oneTag<T extends keyof HTMLElementTagNameMap>(tag: T): SingleQuery<HTMLElementTagNameMap[T]> {
+        const result = this.#node.querySelector(tag);
+        if (result) return new SingleQuery(this.#path + `[oneTag=${tag}]`, result);
+        else throw err(this, `${tag} is not a unique element below ${nodeToStr(this.#node)}!`);
+    }
+
+    someTags<T extends keyof HTMLElementTagNameMap>(tag: T): MultiQuery<HTMLElementTagNameMap[T]> {
+        const result = this.#node.querySelectorAll(tag);
+        if (result.length >= 1)
+            return new MultiQuery(this.#path + `[someTags=${tag}]`, [...result]);
+        else throw err(this, `${tag} not present below ${nodeToStr(this.#node)}!`);
+    }
+
+    anyTags<T extends keyof HTMLElementTagNameMap>(tag: T): MultiQuery<HTMLElementTagNameMap[T]> {
+        const result = this.#node.querySelectorAll(tag);
+        return new MultiQuery(this.#path + `[someTags=${tag}]`, [...result]);
+    }
+}
+
+class SingleQuery<T extends Element> implements ElementQuery<Element> {
     readonly #node: T;
     readonly #path: string;
 
@@ -37,6 +126,12 @@ class SingleQuery<T extends Element> implements Query<Element> {
 
     value(): T {
         return this.#node;
+    }
+
+    withId(id: string) {
+        if (this.#node.id == id)
+            return new SingleQuery(this.#path + `[withId=${id}]`, this.#node);
+        throw err(this, `${nodeToStr(this.#node)} does not have ID ${id}!`);
     }
 
     childWithId(id: string) {
@@ -80,10 +175,15 @@ class SingleQuery<T extends Element> implements Query<Element> {
         else throw err(this, `${className} not present!`);
     }
 
+    anyClasses(className: string) {
+        const result = this.#node.getElementsByClassName(className);
+        return new MultiQuery(this.#path + `[anyClasses=${className}]`, [...result]);
+    }
+
     withClass(className: string) {
-        if (!this.#node.classList.contains(className))
-            throw err(this, `${nodeToStr(this.#node)} does not have class ${className}!`);
-        return new SingleQuery(this.#path + `[withClass=${className}]`, this.#node);
+        if (this.#node.classList.contains(className))
+            return new SingleQuery(this.#path + `[withClass=${className}]`, this.#node);
+        else throw err(this, `${nodeToStr(this.#node)} does not have class ${className}!`);
     }
 
     oneTag<T extends keyof HTMLElementTagNameMap>(tag: T): SingleQuery<HTMLElementTagNameMap[T]> {
@@ -104,9 +204,14 @@ class SingleQuery<T extends Element> implements Query<Element> {
         const result = this.#node.getElementsByTagName(tag);
         return new MultiQuery(this.#path + `[someTags=${tag}]`, [...result]);
     }
+
+    withTag<T extends keyof HTMLElementTagNameMap>(tag: T): SingleQuery<HTMLElementTagNameMap[T]> {
+        if (this.#node.tagName == tag) return new SingleQuery(this.#path + `[withTag=${tag}]`);
+        else throw err(this, `${nodeToStr(this.#node)} does not have tag ${tag}!`);
+    }
 }
 
-class MultiQuery<T extends Element> implements Query<Element[]> {
+class MultiQuery<T extends Element> implements ElementQuery<Element[]> {
     readonly #nodes: T[];
     readonly #path: string;
 
@@ -121,6 +226,14 @@ class MultiQuery<T extends Element> implements Query<Element[]> {
 
     value(): T[] {
         return this.#nodes;
+    }
+
+    withId(id: string) {
+        const result = this.#nodes
+            .filter(node => node.id == id);
+
+        if (result.length == 1) return new SingleQuery(this.#path + `[withId=${id}]`, result[0]);
+        else throw err(this, `${!result.length ? 'No node' : 'More than one node'} has ID ${id} in ${this.#nodes.map(node => nodeToStr(node)).join(',')}!`);
     }
 
     childWithId(id: string): Query<Element> {
@@ -168,10 +281,17 @@ class MultiQuery<T extends Element> implements Query<Element[]> {
     someClasses(className: string) {
         const result = this.#nodes
             .map(node => node.getElementsByClassName(className))
-            .filter(elements => elements.length >= 1)
-            .map(elements => elements[0]);
+            .filter(elements => elements.length >= 1);
 
-        if (this.#nodes.length == result.length) return new MultiQuery(this.#path + `[someClasses=${className}]`, result);
+        if (this.#nodes.length == result.length) return new MultiQuery(this.#path + `[someClasses=${className}]`, result.flatMap(coll => [...coll]));
+        else throw err(this, `${className} not always present on ${this.#nodes.map(node => nodeToStr(node)).join(',')}!`);
+    }
+
+    anyClasses(className: string) {
+        const result = this.#nodes
+            .map(node => node.getElementsByClassName(className));
+
+        if (this.#nodes.length == result.length) return new MultiQuery(this.#path + `[anyClasses=${className}]`, result.flatMap(coll => [...coll]));
         else throw err(this, `${className} not always present on ${this.#nodes.map(node => nodeToStr(node)).join(',')}!`);
     }
 
@@ -196,20 +316,25 @@ class MultiQuery<T extends Element> implements Query<Element[]> {
     someTags<T extends keyof HTMLElementTagNameMap>(tag: T): MultiQuery<HTMLElementTagNameMap[T]> {
         const result = this.#nodes
             .map(node => node.getElementsByTagName(tag))
-            .filter(elements => elements.length >= 1)
-            .map(elements => elements[0]);
+            .filter(elements => elements.length >= 1);
 
-        if (this.#nodes.length == result.length) return new MultiQuery(this.#path + `[someTags=${tag}]`, result);
+        if (this.#nodes.length == result.length) return new MultiQuery(this.#path + `[someTags=${tag}]`, result.flatMap(coll => [...coll]));
         else throw err(this, `${tag} not always present below some node in ${this.#nodes.map(node => nodeToStr(node)).join(',')}!`);
     }
 
     anyTags<T extends keyof HTMLElementTagNameMap>(tag: T): MultiQuery<HTMLElementTagNameMap[T]> {
         const result = this.#nodes
-            .map(node => node.getElementsByTagName(tag))
-            .filter(elements => elements.length >= 1)
-            .map(elements => elements[0]);
+            .map(node => node.getElementsByTagName(tag));
 
-        return new MultiQuery(this.#path + `[someTags=${tag}]`, result);
+        return new MultiQuery(this.#path + `[anyTags=${tag}]`, result.flatMap(coll => [...coll]));
+    }
+
+    withTag<T extends keyof HTMLElementTagNameMap>(tag: T): SingleQuery<HTMLElementTagNameMap[T]> {
+        const result = this.#nodes
+            .filter(node => node.tagName == tag) as unknown as HTMLElementTagNameMap[T][];
+
+        if (result.length == 1) return new SingleQuery(this.#path + `[withTag=${tag}]`, result[0]);
+        else throw err(this, `${!result.length ? 'No node' : 'More than one node'} has tag ${tag} on ${this.#nodes.map(node => nodeToStr(node)).join(',')}!`);
     }
 }
 
@@ -223,7 +348,8 @@ function err(query: Query<never>, message: string) {
     return new ViewIntegrityError(query, message);
 }
 
-function nodeToStr(node: Element) {
+function nodeToStr(node: Element | DocumentFragment) {
+    if (node instanceof DocumentFragment) return '[Fragment]';
     if (node.id) return `${node.tagName}[#${node.id}]`;
     else return '<' + node.tagName + '>';
 }
